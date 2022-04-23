d072057db43794ab6e947333244ed923052d7b8e
jxhnkndl <jkroyston@gmail.com>
2022-04-23 13:54:13 -0400

    complete sum-array algorithm

---
+++ set-2/03-sum-array/Unsolved/sum-array.js
@@ -0,0 +1,11 @@
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
+};
