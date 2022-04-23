c51db7aa92aeef58b8bebf3d01561f48c28a1381
jxhnkndl <jkroyston@gmail.com>
2022-04-23 18:35:32 -0400

    complete max-num algorithm

---
+++ set-3/02-max-num/Unsolved/max-num.js
@@ -0,0 +1,15 @@
+// Write code to return the largest number in the given array
+
+var maxNum = function(arr) {
+  var maxNum = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    var currentNum = arr[i];
+
+    if (currentNum > maxNum) {
+      maxNum = currentNum;
+    }
+  }
+
+  return maxNum;
+};
